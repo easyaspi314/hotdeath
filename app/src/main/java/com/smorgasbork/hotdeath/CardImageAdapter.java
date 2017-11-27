d9e5c8ae0aae463f118258c0f3dd5c59c87f8f07
Devin <easyaspi314@users.noreply.github.com>
2017-11-26 21:18:30 -0500

    Some optimizations and cleanup

--- app/src/main/java/com/smorgasbork/hotdeath/CardImageAdapter.java
+++ app/src/main/java/com/smorgasbork/hotdeath/CardImageAdapter.java
@@ -1,87 +1,99 @@
 package com.smorgasbork.hotdeath;
 
-import java.util.HashMap;
-
-import android.widget.BaseAdapter;
 import android.content.Context;
-import android.view.*;
-import android.widget.ImageView;
+import android.util.SparseBooleanArray;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.GridView;
+import android.widget.ImageView;
 
-public class CardImageAdapter extends BaseAdapter {
+public class CardImageAdapter extends BaseAdapter
+{
     private Context mContext;
-    private Integer[] m_cardIDs;
-    private Integer[] m_thumbIDs;
+    private int[]   m_cardIDs;
+    private int[]   m_thumbIDs;
 
-    public Integer[] getCardIDs()
+    public CardImageAdapter(Context c)
     {
-    	return m_cardIDs;
-    }
-    
-    public CardImageAdapter(Context c) 
-    {
-    	GameActivity ga = (GameActivity)c;
-    	
-    	// query the current card deck to see what cards are actually in use
-    	Game g = ga.getGame();
-    	CardDeck d = g.getDeck();
-    	Card[] cary = d.getCards();
-    	
-    	HashMap<Integer, Boolean> usedIDs = new HashMap<Integer, Boolean>();
-    	for (int i = 0; i < cary.length; i++)
-    	{
-    		if (usedIDs.containsKey(cary[i].getID()))
-    		{
-    			continue;
-    		}
-    		
-    		usedIDs.put(cary[i].getID(), true);
-    	}
+        GameActivity ga = (GameActivity) c;
 
-    	// go through all cards in order and add them to the array
-    	Integer[] cardids = ga.getCardIDs();
-    	
-    	Integer idx = 0;
-    	m_thumbIDs = new Integer[usedIDs.size()];
-    	m_cardIDs = new Integer[usedIDs.size()];
-    	for (int i = 0; i < cardids.length; i++)
-    	{
-    		if (usedIDs.containsKey(cardids[i]))
-    		{
-    			m_cardIDs[idx] = cardids[i];
-    	   		m_thumbIDs[idx] = ((GameActivity)c).getCardImageID(cardids[i]);
-    	   		idx++;
-       		}
-    	}
-    	
+        // query the current card deck to see what cards are actually in use
+        Game g = ga.getGame();
+        CardDeck d = g.getDeck();
+        Card[] cary = d.getCards();
+
+        SparseBooleanArray usedIDs = new SparseBooleanArray();
+        for (Card aCary : cary)
+        {
+            if (usedIDs.indexOfKey(aCary.getID()) >= 0)
+            {
+                continue;
+            }
+
+            usedIDs.put(aCary.getID(), true);
+        }
+
+        // go through all cards in order and add them to the array
+        int[] cardids = ga.getCardIDs();
+
+        Integer idx = 0;
+        m_thumbIDs = new int[usedIDs.size()];
+        m_cardIDs = new int[usedIDs.size()];
+        for (Integer cardid : cardids)
+        {
+            if (usedIDs.indexOfKey(cardid) >= 0)
+            {
+                m_cardIDs[idx] = cardid;
+                m_thumbIDs[idx] = ((GameActivity) c).getCardImageID(cardid);
+                idx++;
+            }
+        }
+
         mContext = c;
     }
 
-    public int getCount() {
+    public int[] getCardIDs()
+    {
+        return m_cardIDs;
+    }
+
+    @Override
+    public int getCount()
+    {
         return m_thumbIDs.length;
     }
 
-    public Object getItem(int position) {
+    /* TODO: I don't think this is the intended usage of these... */
+    @Override
+    public Object getItem(int position)
+    {
         return null;
     }
 
-    public long getItemId(int position) {
+    @Override
+    public long getItemId(int position)
+    {
         return 0;
     }
 
     // create a new ImageView for each item referenced by the Adapter
-    public View getView(int position, View convertView, ViewGroup parent) 
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent)
     {
         ImageView imageView;
-        if (convertView == null) {  // if it's not recycled, initialize some attributes
+        if (convertView == null)
+        {  // if it's not recycled, initialize some attributes
             imageView = new ImageView(mContext);
             imageView.setLayoutParams(new GridView.LayoutParams(85, 85));
             imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
             imageView.setPadding(8, 8, 8, 8);
-        } else {
+        }
+        else
+        {
             imageView = (ImageView) convertView;
         }
-        
+
         imageView.setImageResource(m_thumbIDs[position]);
         return imageView;
     }
--- app/src/main/java/com/smorgasbork/hotdeath/GameOptions.java
+++ app/src/main/java/com/smorgasbork/hotdeath/GameOptions.java
@@ -5,98 +5,99 @@ package com.smorgasbork.hotdeath;
  * This class wraps all the prefs checking; it's a bit of a vestige of this application's
  * PocketPC origins, but it also makes it easier to call Prefs.get*() functions, since it
  * always has handy access to the GameActivity, and thus, the context.
- * @author priebe
  *
+ * @author priebe
  */
-public class GameOptions {
-	
-	private GameActivity m_ga;
-	
-	public GameOptions (GameActivity ga)
-	{
-		m_ga = ga;
-	}
-	
-	public void shutdown ()
-	{
-		m_ga = null;
-	}
+public class GameOptions
+{
 
-	public int getP1Skill() 
+    private GameActivity m_ga;
+
+    public GameOptions(GameActivity ga)
     {
-		return Prefs.getP1SkillLevel(m_ga);
+        m_ga = ga;
     }
 
-	public int getP2Skill() 
+    public void shutdown()
     {
-		return Prefs.getP2SkillLevel(m_ga);
+        m_ga = null;
     }
 
-	public int getP3Skill() 
+    public int getP1Skill()
     {
-		return Prefs.getP3SkillLevel(m_ga);
+        return Prefs.getP1SkillLevel(m_ga);
     }
 
-	public int getP1Agg() 
+    public int getP2Skill()
     {
-		return Prefs.getP1AggressionLevel(m_ga);
+        return Prefs.getP2SkillLevel(m_ga);
     }
 
-	public int getP2Agg() 
+    public int getP3Skill()
     {
-		return Prefs.getP2AggressionLevel(m_ga);
+        return Prefs.getP3SkillLevel(m_ga);
     }
 
-	public int getP3Agg() 
+    public int getP1Agg()
     {
-		return Prefs.getP3AggressionLevel(m_ga);
+        return Prefs.getP1AggressionLevel(m_ga);
     }
 
-	public int getPauseLength() 
+    public int getP2Agg()
     {
-		return Prefs.getGameSpeed(m_ga);
+        return Prefs.getP2AggressionLevel(m_ga);
     }
 
-	public int getCheatLevel() 
+    public int getP3Agg()
     {
-		return Prefs.getCheatLevel(m_ga);
-    }
-	
-	public boolean getFamilyFriendly ()
-	{
-		String s = Prefs.getCheatCode (m_ga);
-		if (s.contains("originalhotdeath"))
-		{
-			return false;
-		}
-		
-		return true;
-	}
-
-	public boolean getFaceUp() 
-    {
-        return Prefs.getFaceUp(m_ga); 
+        return Prefs.getP3AggressionLevel(m_ga);
     }
 
-	public boolean getComputer4th() 
+    public int getPauseLength()
     {
-		return Prefs.getComputer4th(m_ga);
+        return Prefs.getGameSpeed(m_ga);
     }
 
-	public boolean getStandardRules() 
+    public int getCheatLevel()
     {
-		String s = Prefs.getCheatCode (m_ga);
-		if (s.contains("standardrules"))
-		{
-			return true;
-		}
-		
-		return false;
+        return Prefs.getCheatLevel(m_ga);
     }
 
-	public boolean getOneDeck() 
+    public boolean getFamilyFriendly()
     {
-		return !Prefs.getTwoDecks(m_ga);
+        String s = Prefs.getCheatCode(m_ga);
+        if (s.contains("originalhotdeath"))
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public boolean getFaceUp()
+    {
+        return Prefs.getFaceUp(m_ga);
+    }
+
+    public boolean getComputer4th()
+    {
+        return Prefs.getComputer4th(m_ga);
+    }
+
+    public boolean getStandardRules()
+    {
+        String s = Prefs.getCheatCode(m_ga);
+        if (s.contains("standardrules"))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public boolean getOneDeck()
+    {
+        return !Prefs.getTwoDecks(m_ga);
     }
 
 }
